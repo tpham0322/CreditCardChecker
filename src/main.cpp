c316f9a92cf67e419d580ef8a4e71629db83da25
Truong Pham <truongpham1173@gmail.com>
2024-07-10 19:22:30 -0400

    Header comment

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,9 @@
+// Author: Truong Pham
+// Created July 10 2024
+//
+// Purpose of this program is to see if a credit card number is valid
+// and find out what type of card it is, American Express, Visa, etc.
+
 #include <iostream>
 
 
